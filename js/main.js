f96076a3d149aef1bf6795556cfb7b01959a3b08
kaifgour <kaifi252000@gmail.com>
2024-06-22 18:50:27 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active-menu');
+    });
+});
